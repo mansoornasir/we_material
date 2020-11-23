7955849ff860ff907d838649303e8fc7d88fc8b5
Taimur Aamer <1t.aamer@gmail.com>
2020-11-24 01:37:52 +0500

    added css

---
+++ 4 - Javascript/0 - Bascis/basics.js
@@ -0,0 +1,36 @@
+// a = 10
+// b = 20
+// c = 9.0
+// name = "Wadood"
+// dmc = {
+//     name: name,
+//     gpa: 3.4,
+//     marks: 90,
+//     address:"Hungu"
+// }
+
+// if(typeof name === "string")
+
+// console.log(typeof dmc)
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+// result = sum(10,30);
+// console.log(result)
+
+
+// dmc = {
+//     name: "Abdul Wadood",
+//     gpa: 3.4,
+//     marks: 90,
+//     address:"Hungu"
+// }
+
+// console.log(dmc.marks)
+
+console.log('sanity test')
+
+
