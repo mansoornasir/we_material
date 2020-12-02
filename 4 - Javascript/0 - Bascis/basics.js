92293d5ea0ac33d1f66d971d2bcefbc69ad63fd6
Mansoor Nasir <mrmansoornasir@gmail.com>
2020-12-02 16:32:41 +0500

    javascript

--- 4 - Javascript/0 - Bascis/basics.js
+++ 4 - Javascript/0 - Bascis/basics.js
@@ -1,36 +1,87 @@
-// a = 10
-// b = 20
-// c = 9.0
-// name = "Wadood"
-// dmc = {
-//     name: name,
-//     gpa: 3.4,
-//     marks: 90,
-//     address:"Hungu"
+var age = 100;
+
+// if (age >= 1 && age < 20) {
+//     console.log('Adolesent');
+// }
+// if (age >= 20 && age <= 60) {
+//     console.log('Adulthood');
+// }
+// if (age > 60) {
+//     console.log('Old Age');
 // }
 
-// if(typeof name === "string")
+// var a = 10;
+// if (a > 2)
+//     console.log('Yes');
+// else if (a > 3)
+//     console.log("No")
+// else if (a > 5)
+//     console.log("True")
+// else
+//     console.log('else part')
 
-// console.log(typeof dmc)
+// var ch = window.prompt('Enter a character');
 
-// function sum(a, b) {
-//     return a + b;
+// switch (ch) {
+//     case 'a':
+//     case 'e':
+//     case 'i':
+//     case 'o':
+//     case 'u':
+//         console.log('Vowel');
+//         break;
+//     default:
+//         console.log('Consonenet')
 // }
 
+// var a = 10;
 
-// result = sum(10,30);
-// console.log(result)
-
-
-// dmc = {
-//     name: "Abdul Wadood",
-//     gpa: 3.4,
-//     marks: 90,
-//     address:"Hungu"
+// if (a > 10) {
+//     console.log(10);
+// }
+// else {
+//     console.log(30)
 // }
 
-// console.log(dmc.marks)
+// test = (a > 10 ? 20 : 30);
 
-console.log('sanity test')
+// console.log(test);
 
+// a = a + 1;
 
+// for (i = 0; i <= 20; i += 1) {
+//     if (i % 2 != 0)
+//         console.log(i);
+// }
+
+// list = [30, 50, 60, 80, 40, 60, 90, 100]
+
+// for (i = 0; i < list.length; i++) {
+//     console.log(list[i])
+// }
+
+// for (index in list) {
+//     console.log(list[index])
+// }
+
+// a = 90;
+
+// while (a <= 100) {
+//     console.log(a)
+//     a++;
+// }
+
+// i = 100;
+
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// }
+
+i = 10;
+
+do {
+    console.log(i)
+    i++;
+}
+while (i <= 20);
